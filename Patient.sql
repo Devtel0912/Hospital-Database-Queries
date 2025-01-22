f1bcd23aebf584f2ef7c12920f98b244b870b855
Dev Telang <77637700+Devtel0912@users.noreply.github.com>
2025-01-22 15:39:56 -0500

    Add files via upload
    This shows how to make the table for patient

---
+++ Patient.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Patient
+(PatientID int Primary Key,
+ Name varchar(50),
+ Address varchar(50),
+ Phone_Number varchar(15),
+ Email varchar(50),
+ Location varchar(50),
+ Date_of_birth datetime
+);
